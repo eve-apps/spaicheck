c9bc8ebc844d0b4143433d565a31c42054f56628
Alexander Roberts <detrohutt@gmail.com>
2015-11-20 16:17:14 -0500

    Add working prevalidation; No API call made unless form is valid

--- client/addKey.js
+++ client/addKey.js
@@ -1,15 +1,39 @@
+AddKeyFormSchema = new SimpleSchema({
+  keyID: {
+    type: Number,
+    label: "Key ID",
+    min: 0,
+    max: 2147483647
+  },
+  vCode: {
+    type: String,
+    label: "Verification Code",
+    regEx: /^[0-9a-zA-Z]+$/
+  }
+})
+
+AutoForm.debug();
+
 AutoForm.hooks({
   insertKeyForm: {
     before: {
       insert: function(doc) {
-        let self = this;
-        console.log(doc);
-        Meteor.apply('validateKey', [doc.keyID, doc.vCode], true, function(err, result) {
-          if (err) self.result(false);
-          doc.status = result;
-          console.log(doc.status);
-          self.result(doc);
-        });
+        this.event.preventDefault();
+        formIsValid = AutoForm.validateForm('insertKeyForm');
+
+        if (formIsValid) {
+          let self = this;
+          Meteor.apply('validateKey', [doc.keyID, doc.vCode], true, function(err, result) {
+            if (err) {
+              self.result(false);
+            }
+            else {
+              doc.status = result.result;
+              self.result(doc);
+            }
+          });
+        }
+        else this.result(false);
       }
     }
   }
