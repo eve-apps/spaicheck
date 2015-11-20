c855bf43c9119307e13096ad29647cc721018ad5
Alexander Roberts <detrohutt@gmail.com>
2015-11-20 06:13:09 -0500

    Add createdAt prop and status object to schema

--- collections/Keys.js
+++ collections/Keys.js
@@ -1,5 +1,24 @@
 Keys = new Mongo.Collection("keys");
 
+StatusSchema = new SimpleSchema({
+    ok: {
+      type: Boolean,
+      optional: true
+    },
+    reasons: {
+      type: [String],
+      optional: true
+    },
+    lastChecked: {
+      type: Date,
+      optional: true
+    },
+    error: {
+      type: String,
+      optional: true
+    }
+});
+
 KeySchema = new SimpleSchema({
   keyID: {
     type: Number,
@@ -13,11 +32,28 @@ KeySchema = new SimpleSchema({
     custom: function() {
       if (this.field("keyID").isSet === false) return "keyIDMissing";
       if (this.field("keyID").value < 0) return "keyIDInvalid";
-
-      keyValidationResult = Meteor.apply('validateKey', [this.field("keyID").value, this.value], true);
-      console.log(keyValidationResult);
-      if (keyValidationResult.ok === true) return 0;
-      else return "keyFailed";
+    }
+  },
+  createdAt: {
+    type: Date,
+    autoValue: function() {
+      if (this.isInsert) {
+        return new Date;
+      } else if (this.isUpsert) {
+        return {$setOnInsert: new Date};
+      } else {
+        this.unset();  // Prevent user from supplying their own value
+      }
+    },
+    autoform: {
+      omit: true
+    }
+  },
+  status: {
+    type: StatusSchema,
+    // optional: true,
+    autoform: {
+      omit: true
     }
   }
 });
