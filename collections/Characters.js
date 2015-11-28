6cc3c0783c93639e679d54e5b0b3c1a8ed3b8183
Soullesswaffle <mickeydekkers@gmail.com>
2015-11-28 14:19:46 +0100

    Add Characters collection

---
+++ collections/Characters.js
@@ -0,0 +1,82 @@
+// The Mongo collection to which our schema will be attached
+Characters = new Mongo.Collection("characters");
+
+EmploymentRecordSchema = new SimpleSchema({
+  recordID: String,
+  corporationID: String,
+  corporationName: String,
+  startDate: Date
+});
+
+ApplicationSchema = new SimpleSchema({
+  applicationID: {
+    type: String,
+    index: true,
+    unique: true,
+    sparse: true
+  },
+  message: String,
+  submittedAt: Date,
+  processedAt: {
+    type: Date,
+    optional: true
+  }
+});
+
+CharacterSchema = new SimpleSchema({
+  characterID: {
+    type: String,
+    index: true,
+    unique: true
+  },
+  characterName: {
+    type: String,
+    optional: true
+  },
+  corporationID: {
+    type: String,
+    optional: true
+  },
+  corporationName: {
+    type: String,
+    optional: true
+  },
+  allianceID: {
+    type: String,
+    optional: true
+  },
+  allianceName: {
+    type: String,
+    optional: true
+  },
+  securityStatus: {
+    type: String,
+    optional: true
+  },
+  employmentHistory: {
+    type: [EmploymentRecordSchema],
+    optional: true
+  },
+  keyID: { // Associated API key
+    type: String,
+    optional: true
+  },
+  applications: { // Associated membership applications
+    type: [ApplicationSchema],
+    optional: true
+  },
+  createdAt: { // When the character was first added to the database
+    type: Date,
+    autoValue: function() {
+      if (this.isInsert) {
+        return new Date;
+      } else if (this.isUpsert) {
+        return $setOnInsert {new Date};
+      } else {
+        this.unset();  // Prevent user from supplying their own value
+      }
+    }
+  }
+});
+
+Characters.attachSchema(CharacterSchema);
