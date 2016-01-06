c5061d138b4af750b09c983de9df8953e54ca9d4
Soullesswaffle <mickeydekkers@gmail.com>
2016-01-06 22:38:46 +0100

    WIP pretty emails

--- server/handleChanges.js
+++ server/handleChanges.js
@@ -2,25 +2,25 @@ const jsonPatch = Meteor.npmRequire('fast-json-patch');
 
 Meteor.methods({
   'handleChanges': function (keyID, err, result) {
-    if (err) {
-      console.log(err);
-      if (err.error == 'INVALIDKEY') {
-        Meteor.call('notifyChanges', keyID, err.reason);
+    if (!err || err.error == 'INVALIDKEY') {
+      let oldRecord = Keys.findOne({keyID: keyID}).resultBody;
+      if (err && err.error == 'INVALIDKEY') {
+        Meteor.call('notifyChanges', keyID, oldRecord, err.reason);
+        //TODO: remove key from DB
+      } else if (result.statusFlags[0] != 'GOOD') {
+        Meteor.call('notifyChanges', keyID, oldRecord, result.statusFlags);
         //TODO: remove key from DB
       }
-    }
-    else if (result.statusFlags[0] != 'GOOD') {
-      Meteor.call('notifyChanges', keyID, 'Key no longer passes corporation checks - ' + result.statusFlags);
-      //TODO: remove key from DB
-    }
-    else {
-      let oldRecord = Keys.findOne({keyID: keyID}).resultBody;
-      let newRecord = _.omit(result, ['currentTime', 'cachedUntil', 'statusFlags']);
-      let diff = jsonPatch.compare(oldRecord, newRecord);
+      else {
+        let newRecord = _.omit(result, ['currentTime', 'cachedUntil', 'statusFlags']);
+        let diff = jsonPatch.compare(oldRecord, newRecord);
 
-      if (diff.length == 0) return 0;
-      Meteor.call('notifyChanges', keyID, diff);
-      //TODO: decide whether to remove key from DB
+        if (diff.length == 0) return 0;
+        Meteor.call('notifyChanges', keyID, oldRecord, diff);
+        //TODO: decide whether to remove key from DB
+      }
+    } else {
+      console.log(err);
     }
   }
 });
--- server/notifyChanges.js
+++ server/notifyChanges.js
@@ -1,5 +1,5 @@
 Meteor.methods({
-  'notifyChanges': function (key, changes) {
+  'notifyChanges': function (key, before, changes) {
     // Let other method calls from the same client start running,
     // without waiting for the email sending to complete.
     this.unblock();
@@ -8,7 +8,7 @@ Meteor.methods({
       to: mailTo,
       from: "changes@spaicheck.com",
       subject: "Key #" + key + " has changed",
-      text: changes
+      text: changes + '\n\n\n' + JSON.stringify(before)
     });
   }
 });
