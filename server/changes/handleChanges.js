b0755e51f8ab516e86aebfc1537a7d5f774453d2
Alexander Roberts <detrohutt@gmail.com>
2016-01-07 15:46:30 -0500

    Add logic to detect and classify changes to existing keys

--- server/changes/handleChanges.js
+++ server/changes/handleChanges.js
@@ -17,15 +17,52 @@ Meteor.methods({
       let oldRecord = Keys.findOne({keyID: keyID}).resultBody.characters;
       let newRecord = result.characters;
       let diff = jsonPatch.compare(oldRecord, newRecord);
+
       console.log(diff);
       // Iterate over the diff array, handling every possible change of importance to the corp
-      for (let change in diff) {
+      diff.forEach(function (change, index) {
         // 'replace' ops can only logically happen inside a single character(#) object
         if (change.op == 'replace') {
-
+          // RegEx to catch changes to corporationID or allianceID
+          let charChangePattern = /\/(\d+)\/(corporationID|allianceID)/;
+          // After test, charChangeTest will hold remembered values for characterID and changed field
+          // exec() returns an array where first item is the matched string, all other items are remembered values
+          let charChangeTest = charChangePattern.exec(diff[index].path);
+          // Make sure that the test was successful and if so, pull out remembered values
+          if (charChangeTest && charChangeTest[0]) {
+            let curCharID = charChangeTest[1];
+            let curFieldName = charChangeTest[2];
+            // Console log newChanges to get an idea for how it works
+            newChanges.push({
+              type: curFieldName,
+              data: {
+                old: oldRecord[curCharID][curFieldName],
+                new: change.value
+              }
+            });
+          }
         }
-      }
+        // 'add' and 'remove' ops can only logically happen at the 'characters' level
+        else if (change.op == 'add' || change.op == 'remove') {
+          // The following works very similarly to the "if" block above. Look there for more help.
+          let charAddRemovePattern = /\/(\d+)/;
+          let charAddRemoveTest = charAddRemovePattern.exec(diff[index].path);
+          if (charAddRemoveTest && charAddRemoveTest[0]) {
+            let curCharID = charAddRemoveTest[1];
+
+            newChanges.push({
+              type: change.op + "Character",
+              // Depending on which type of operation, either 'old'('add' op) or 'new'('remove' op) will be undefined
+              data: {
+                old: oldRecord[curCharID],
+                new: change.value
+              }
+            });
+          }
+        }
+      });
     }
+    console.log(newChanges);
     if (newChanges.length != 0) Meteor.call('notifyChanges', keyID, newChanges.toString());
   }
 });
