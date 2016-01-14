e97d632109cf7dc518cab91acea30294fbbc319a
Soullesswaffle <mickeydekkers@gmail.com>
2016-01-15 00:24:59 +0100

    Use Meteor's settings.json system instead of private.tmpl.js

--- server/lib/env.js
+++ server/lib/env.js
@@ -1,9 +1,9 @@
 Meteor.startup(function () {
-  if (mailUser !== 'undefined' && mailPass !== 'undefined' && mailServer !== 'undefined' && mailPort !== 'undefined') {
-    console.log("Vars Set");
-    process.env.MAIL_URL = "smtp://" + mailUser + ":" + mailPass + "@" + mailServer + ":" + mailPort;
+  if (_.every(["mailUser", "mailPass", "mailServer", "mailPort"], function (mailSetting) { return Meteor.settings.private[mailSetting] != null; })) {
+    console.log("Email configured.");
+    process.env.MAIL_URL = "smtp://" + Meteor.settings.private.mailUser + ":" + Meteor.settings.private.mailPass + "@" + Meteor.settings.private.mailServer + ":" + Meteor.settings.private.mailPort;
   }
   else {
-    console.log("Emails will not be sent until you provide the required information in the server/private.tmpl.js file");
+    console.log("Emails will not be sent until you provide the required information in the settings.json file");
   }
 });
