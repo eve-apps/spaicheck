1d4737f694c8a2801d882e66dd55b14640cda12f
Soullesswaffle <mickeydekkers@gmail.com>
2016-01-15 21:13:58 +0100

    Remove insecure, start defining Security rules

--- server/security.js
+++ server/security.js
@@ -14,3 +14,26 @@ let trustedOrigins = [
 _.each(trustedOrigins, function (to) {
   BrowserPolicy.content.allowOriginForAll("https://" + to);
 });
+
+Security.defineMethod("ifOnWhitelist", {
+  fetch: [],
+  transform: null,
+  deny: function (type, arg, userID, doc, fields, modifier) {
+    let res =  Meteor.user() ? !Whitelist.findOne({characterID: String(Meteor.user().profile.eveOnlineCharacterId)}) : true;
+    console.log("SECURITY - WHITELIST:", (res ? "DENY" : "ALLOW"), userID, type, doc, fields, modifier);
+    return res;
+  }
+});
+
+Security.defineMethod("ifAdmin", {
+  fetch: [],
+  transform: null,
+  deny: function (type, arg, userID, doc, fields, modifier) {
+    let res =  Meteor.user() ? Meteor.user().profile.eveOnlineCharacterId !== Meteor.settings.public.adminID : true;
+    console.log("SECURITY - ADMIN:", (res ? "DENY" : "ALLOW"), userID, type, doc, fields, modifier);
+    return res;
+  }
+});
+
+Security.permit(["insert", "update", "remove"]).collections([Changes, Characters, Errors, Keys, Whitelist]).ifOnWhitelist().apply();
+Security.permit(["insert", "update", "remove"]).collections([Changes, Characters, Errors, Keys, Whitelist]).ifAdmin().apply();
