23a9de1666bc9bc59cda15b2d82ea22150fe9adc
Alexander Roberts <detrohutt@gmail.com>
2016-01-16 15:30:01 -0500

    Merge 'Validate Keys' section into the Key Explorer

--- client/templates/home.js
+++ client/templates/home.js
@@ -12,15 +12,3 @@ Template.home.onCreated(function () {
 Template.home.onDestroyed(function () {
   Meteor.clearInterval(eachSecond);
 });
-
-/**
- * Event Handlers
- **/
-
-Template.validateKeys.events({
-  "click .validate-button": function() {
-    // Fetch all keys from the database and validate them
-    // TODO: Use "Async" library to process these in parallel
-    Meteor.call('runChecks');
-  }
-});
--- client/templates/keyDisplay.js
+++ client/templates/keyDisplay.js
@@ -154,13 +154,18 @@ handleDetailsClick = function (evType, ev) {
   **/
 
  Template.keyDisplay.events({
-   "click .rm-key": function() {
-     Keys.remove(Keys.findOne({keyID: this.keyID})._id)
+   "click .validate-button": function() {
+     // Fetch all keys from the database and validate them
+     // TODO: Use "Async" library to process these in parallel
+     Meteor.call('runChecks');
    },
    "click .toggle.changesBtn": function(ev) {
      handleDetailsClick('CHANGES', ev);
    },
    "click .toggle.detailsBtn": function(ev) {
      handleDetailsClick('KEYINFO', ev);
-   }
+   },
+   "click .rm-key": function() {
+     Keys.remove(Keys.findOne({keyID: this.keyID})._id)
+   },
  });
