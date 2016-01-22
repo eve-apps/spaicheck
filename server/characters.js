fff3b924eaee4011d7c54b311b8af84dbe61f60f
Alexander Roberts <detrohutt@gmail.com>
2016-01-22 18:20:31 -0500

    Implement primary character detection(seems good but needs testing) and auto-set on key in DB

--- server/characters.js
+++ server/characters.js
@@ -6,6 +6,7 @@ Meteor.methods({
       for (let charID in charactersObj) {
         Meteor.call('insertCharacter', keyID, vCode, charID);
       }
+      Meteor.call('detectPrimaryCharacter', keyID);
       done(null, true);
     });
 
@@ -30,19 +31,26 @@ Meteor.methods({
     }
   },
 
-  'detectPrimaryCharacter': function (keyObj) {
-    // eveonlinejs.fetch('server:ServerStatus', {}, function (err, result) {
-    let runSyncResult = Async.runSync(function (done) {
-      charactersObj = keyObj.resultBody.characters;
-      for (let charID in charactersObj) {
-        eveonlinejs.fetch('eve:CharacterInfo', {keyID: keyObj.keyID, vCode: keyObj.vCode, characterID: charID}, function (err, result) {
-          if (err) done(err, null);
-          else done(null, result);
-        });
-      }
-    })
-    if (runSyncResult.error) throw runSyncResult.error;
-    else if (runSyncResult.result) return runSyncResult.result.characterName;
+  'detectPrimaryCharacter': function (keyID) {
+    let charList = Characters.find({keyID: keyID}).fetch();
+
+    charList.sort(function (a, b) {
+      let aInCorporation = a.corporationID == Meteor.settings.public.corporationID;
+      let bInCorporation = b.corporationID == Meteor.settings.public.corporationID;
+      let aInAlliance = a.allianceID == Meteor.settings.public.allianceID;
+      let bInAlliance = b.allianceID == Meteor.settings.public.allianceID;
+
+      if (aInCorporation && !bInCorporation) return -1;
+      else if (bInCorporation && !aInCorporation) return 1;
+      else if (aInAlliance && !bInAlliance) return -1;
+      else if (bInAlliance && !aInAlliance) return 1;
+      else if (a.skillPoints > b.skillPoints) return -1;
+      else if (b.skillPoints > a.skillPoints) return 1;
+    });
+
+    let primaryChar = charList[0].characterName;
+    Meteor.call('setPrimaryCharacter', keyID, primaryChar)
+    return primaryChar;
   },
   'setPrimaryCharacter': function (keyID, charName) {
     Keys.update(
@@ -53,5 +61,6 @@ Meteor.methods({
         }
       }
     );
+    console.log('Primary character for key #' + keyID + ' has been set to ' + charName + '.');
   }
 });
