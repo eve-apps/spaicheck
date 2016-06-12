f08b524b0fe41f3b3b61a808275313ab0440328f
Alexander Roberts <detrohutt@gmail.com>
2016-06-12 11:20:02 -0400

    Add backend code for bulk key import

--- server/keys.js
+++ server/keys.js
@@ -92,6 +92,30 @@ Meteor.methods({
     });
   },
 
+  'insertKeysBulk': function (csvData) {
+    const lines = csvData.split('\n');
+    lines.shift(); // Discard the column headers, we make our own
+
+    const keyObjs = lines.map((line) => {
+      const keyPair = line.split(',');
+
+      return {
+        keyID: keyPair[0],
+        vCode: keyPair[1]
+      }
+    });
+
+    let curTimeout = 0;
+
+    keyObjs.forEach((keyObj) => {
+      // Limit calls to 30 per second by staggering them by 1 30th of a second
+      Meteor.setTimeout(function () {
+        Meteor.call('insertKey', keyObj);
+      // }, curTimeout += Math.ceil(1000/30));
+    }, curTimeout += Math.ceil(1000/5));
+    });
+  },
+
   'removeKey': function (keyID) {
     Changes.remove({keyID: keyID});
     Characters.remove({keyID: keyID});
