b68039875449be710fb65c5c87268aea6a96a37c
Soullesswaffle <mickdekkersnl@gmail.com>
2016-10-15 15:13:17 +0200

    Fix TypeErrors in whitelist helpers

--- imports/api/whitelist/helpers.js
+++ imports/api/whitelist/helpers.js
@@ -1,12 +1,22 @@
 import { Meteor } from 'meteor/meteor';
 import Whitelist from '/imports/api/whitelist/Whitelist';
 
-export const getUserCharacterId = userId => Meteor.users.findOne(userId).profile.eveOnlineCharacterId;
-export const getUserCharacterName = userId => Meteor.users.findOne(userId).profile.eveOnlineCharacterName;
+export const getUserCharacterId = (userId) => {
+  const user = Meteor.users.findOne(userId);
+  if (!user) return null;
+  return user.profile.eveOnlineCharacterId;
+};
+
+export const getUserCharacterName = (userId) => {
+  const user = Meteor.users.findOne(userId);
+  if (!user) return null;
+  return user.profile.eveOnlineCharacterName;
+};
 
 export const characterIsAdmin = characterID => characterID === Meteor.settings.public.adminID;
 export const userIsAdmin = userId => characterIsAdmin(getUserCharacterId(userId));
 
+// FIXME: If the user is a character called 'null', this will think they're on the whitelist.
 export const characterIsWhitelisted = characterID => Boolean(Whitelist.findOne({ characterID: String(characterID) }));
 export const userIsWhitelisted = userId => characterIsWhitelisted(getUserCharacterId(userId));
 
