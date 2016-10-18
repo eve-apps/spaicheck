f05bab96f47fe836d50abfd4440b6b9b6dc91bd0
Soullesswaffle <mickdekkersnl@gmail.com>
2016-10-18 17:02:14 +0200

    Fix TypeError when no changes could be found for a key

--- imports/api/changes/getUnreviewedChanges.js
+++ imports/api/changes/getUnreviewedChanges.js
@@ -1,10 +1,17 @@
 import Changes from '/imports/api/changes/Changes';
 
 const getUnreviewedChanges = (keyID) => {
-  let { log, ...c } = Changes.findOne({ keyID });
-  // Return only unreviewed logs
-  log = log.filter(l => !l.reviewed);
-  return [{ log, ...c }];
+  try {
+    let { log, ...c } = Changes.findOne({ keyID });
+    // Return only unreviewed logs
+    log = log.filter(l => !l.reviewed);
+    return [{ log, ...c }];
+  } catch (error) {
+    // Query returned null
+    if (error instanceof TypeError) return [];
+    console.error(error.stack);
+    return [];
+  }
 };
 
 export default getUnreviewedChanges;
